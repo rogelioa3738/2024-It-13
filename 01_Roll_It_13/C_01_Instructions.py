34dd6c33f123d9baf35004e5b3f99ce5333f2a51
rogelioa3738 <149015162+rogelioa3738@users.noreply.github.com>
2024-02-14 10:56:42 +1300

    Basic instructions and simple yes / no function

---
+++ 01_Roll_It_13/C_01_Instructions.py
@@ -0,0 +1,16 @@
+print("🎲🎲 Roll it 13 🎲🎲 ")
+
+
+# loop for testing purposes
+
+while True:
+    want_instructions = input("Do you wan to read the instructions? ").lower()
+
+    if want_instructions == "yes":
+        print("you chose yes")
+    elif want_instructions == "no":
+        print("you  chose no")
+    else:
+        print("You did not choose a valid response")
+
+ 
